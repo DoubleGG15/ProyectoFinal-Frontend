35ab825209e2fd49647204fbe37b32db37a558e4
DoubleGG15 <giannimelgar3@gmail.com>
2026-06-10 23:14:42 -0600

    Estructura inicial del proyecto angular y al gitignore

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('EjemploPractico');
+    export class App {
+
+contador: number=0;
+idiomaActual: 'es'|'en'='es';
+mensajeAlerta: string='';
+
+incrementarContador():void{
+  this.contador++;
 }
+
+reiniciarContador():void{
+  this.contador=0;
+}
+
+seleccionarIdioma(nuevoIdioma:'es'|'en'):void{
+  this.idiomaActual=nuevoIdioma;
+}
+  clickFondoTarjeta():void {
+  this.mensajeAlerta='Hiciste clic en el fondo de la tarjeta'
+  }
+
+  clickBtnHijo(evento:MouseEvent):void{
+  evento.stopPropagation();
+
+  this.mensajeAlerta='Hiciste un click en el boton interno, el padre no se entero (el fondo amarillo)';
+  }
+
+}
+
+
