f34962059e41265f593eb36e1da772c749d867e4
JhonnyyUrb <jhonny_urbina@unitec.edu>
2026-06-17 18:48:23 -0600

    esperando que funcione

--- src/app/pages/admin/dashboard/dashboard.ts
+++ src/app/pages/admin/dashboard/dashboard.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+
 import { AdminService } from '../../../services/admin.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -10,20 +12,20 @@ import { AdminService } from '../../../services/admin.service';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-
 export class AdminDashboardComponent implements OnInit {
-
   totalUsuarios: number = 0;
   totalCasos: number = 0;
   errorMessage: string = '';
 
-  constructor(private adminService: AdminService) {}
+  constructor(
+    private adminService: AdminService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.adminService.obtenerDashboard().subscribe({
       next: (data: any) => {
-        console.log('DASHBOARD DATA:', data);
-
         this.totalUsuarios = data.TotalUsuarios;
         this.totalCasos = data.TotalCasos;
       },
@@ -32,5 +34,10 @@ export class AdminDashboardComponent implements OnInit {
         this.errorMessage = 'Error al cargar el dashboard';
       }
     });
+  }
+
+  cerrarSesion(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
